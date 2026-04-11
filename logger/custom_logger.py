7dd49680aa4bc55350e5f0bf3c42ca76fb623d8a
AkshayAmbewadkar73 <akshayambewadkar7350pillu@gmail.com>
2026-04-11 18:43:48 +0530

    custom exception has beed done

--- logger/custom_logger.py
+++ logger/custom_logger.py
@@ -61,27 +61,27 @@ from datetime import datetime
 
 class CustomLogger:
     """
-    CustomLogger provides a structured logging mechanism using structlog.
+    CustomLogger provides a structured logging mechanism.
     """
-    def __init__(self,log_dir="logs"):
-        self.log_dir=os.path.join(os.getcwd(),log_dir)
-        os.makedirs(self.log_dir,exist_ok=True)
-        log_file=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-        self.log_file_path=os.path.join(self.log_dir,log_file)
 
-        self.logging.basicConfig(
+    def __init__(self, log_dir="logs"):
+        self.log_dir = os.path.join(os.getcwd(), log_dir)
+        os.makedirs(self.log_dir, exist_ok=True)
+
+        log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+        self.log_file_path = os.path.join(self.log_dir, log_file)
+
+        logging.basicConfig(
             filename=self.log_file_path,
             format="[%(asctime)s ]%(levelname)s %(name)s (line:%(lineno)d) - %(message)s",
             level=logging.INFO,
-
         )
 
+    def get_logger(self, name=__file__):
+        return logging.getLogger(os.path.basename(name))
 
-        
-    def get_logger(self,name=__file__):
-        return self.logging.getLogger(os.path.basename(name))
 
-if __name__=="__main__":
-    logger=CustomLogger().get_logger(__file__)
+if __name__ == "__main__":
+    logger = CustomLogger().get_logger(__file__)
     logger.info("This is an info message")
 
