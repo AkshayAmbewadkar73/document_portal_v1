98ef1c9b842b6a7ebc65a5bfaaacd66af6958de0
AkshayAmbewadkar73 <akshayambewadkar7350pillu@gmail.com>
2026-04-21 20:43:15 +0530

    data analysis done

--- model/models.py
+++ model/models.py
@@ -1,29 +1,44 @@
-from pydantic import BaseModel, Field,RootModel
-from typing import Optional, List, Dict, Any, Union
+# from pydantic import BaseModel, Field,RootModel
+# from typing import Optional, List, Dict, Any, Union
+# from enum import Enum
+
+
+# class Metadata(BaseModel):
+#     summary: List[str] = Field(default_factory=list, description="Summary of the document")
+#     title: Optional[str] = None
+#     author: Optional[str] = None
+#     date_created: Optional[str] = None
+#     last_modified_date: Optional[str] = None
+#     publisher: Optional[str] = None
+#     language: Optional[str] = None
+#     page_count: Optional[Union[int, str]] = None
+#     sentiment_tone: Optional[str] = None
+
+
+# class ChangeFormat(BaseModel):
+#     Page:str
+#     changes:str
+
+# class SummaryResponse(RootModel[List[ChangeFormat]]):
+#    pass
+
+# class PromptType(str, Enum):
+#     DOCUMENT_ANALYSIS = "document_analysis"
+#     DOCUMENT_COMPARISON = "document_comparison"
+#     CONTEXTUALIZE_QUESTION = "contextualize_question"
+#     CONTEXT_QA = "context_qa"
+
+from pydantic import BaseModel,Field,RootModel
+from typing import Optional,List,Dict,Any,Union
 from enum import Enum
 
-
 class Metadata(BaseModel):
-    summary: List[str] = Field(default_factory=list, description="Summary of the document")
-    title: Optional[str] = None
-    author: Optional[str] = None
-    date_created: Optional[str] = None
-    last_modified_date: Optional[str] = None
-    publisher: Optional[str] = None
-    language: Optional[str] = None
-    page_count: Optional[Union[int, str]] = None
-    sentiment_tone: Optional[str] = None
-
-
-class ChangeFormat(BaseModel):
-    Page:str
-    changes:str
-
-class SummaryResponse(RootModel[List[ChangeFormat]]):
-   pass
-
-class PromptType(str, Enum):
-    DOCUMENT_ANALYSIS = "document_analysis"
-    DOCUMENT_COMPARISON = "document_comparison"
-    CONTEXTUALIZE_QUESTION = "contextualize_question"
-    CONTEXT_QA = "context_qa"
+    summary:List[str]=Field(default_factory=list,description="Summary of the document")
+    title:Optional[str]=None
+    author:Optional[str]=None
+    date_created:Optional[str]=None
+    last_modified_date:Optional[str]=None
+    publisher:Optional[str]=None
+    language:Optional[str]=None
+    page_count:Optional[Union[int,str]]=None
+    sentiment_tone:Optional[str]=None
